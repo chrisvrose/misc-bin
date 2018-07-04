0c67c256a1c991500ed2cbb613c3912eadad4d5e
Christopher Rose <christophervrose@gmail.com>
2018-07-04 22:16:13 +0530

    Basic framework

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,1 +1,26 @@
 #nyeh
+def set_board(board, choice):
+	return False
+
+
+def main():
+	board = [[0,0,0],[0,0,0],[0,0,0]]
+	while True:
+		choice = int(input("1-9 for input as per numpad, 0 for exit:"))
+		# Check if input in the numpad range or u mad
+		if choice in range(10):
+			# Exit, since user input
+			if choice == 0:
+				break
+			#Check if input was valid
+			elif set_board(board,choice):
+				print("stuff")
+			#
+			else:
+				print("There's already something there man")
+		else:
+			print("Invalid input")
+	print("hi")
+
+
+main()
