39478b0c0b6b5507fc79d3380f1221f34bf2292f
Christopher Rose <christophervrose@gmail.com>
2018-07-05 13:02:23 +0530

    Choice implemented

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,6 +1,9 @@
 #nyeh
 
 
+# 0 is unplayed, 1 is O and 2 is X
+
+
 # set board
 def set_board(board, choice):
 	position = get_pos(choice)
@@ -26,9 +29,17 @@ def get_pos(choice):
 	return (a,b)
 
 def main():
+	# Init the precious board
 	board = [[0,0,0],[0,0,0],[0,0,0]]
-	turn = 1
+
+	# Ask the player
+	choice_p1 = input("Player 1 - O or X?").upper()
+	turn = 1 if choice_p1=="O" else 2
+
 	while True:
+		# Print current board
+		print_board()
+
 		choice = int(input("1-9 for input as per numpad, 0 for exit:"))
 		# Check if input in the numpad range or u mad
 		if choice in range(10):
