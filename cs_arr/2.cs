908dd7c99c2ffbdefd4497b1ad7635057a761ea8
Christopher V. Rose <christophervrose@gmail.com>
2018-08-19 21:02:41 +0530

    Create 2.cs

---
+++ cs_arr/2.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private int num;
+        public Program(int n)
+        {
+            num = n;
+        }
+        public string Addable()
+        {
+            string returnable = "";
+            string num_string = num.ToString();
+            foreach(char i in num_string)
+                returnable = returnable + (int.Parse(i.ToString()) + 1).ToString();
+            return returnable;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            #region Length Input
+            Console.WriteLine("Enter array length");
+            int a_length;
+            if (int.TryParse(Console.ReadLine(), out a_length));
+            else a_length = 5;
+            #endregion
+
+            int[] a = new int[a_length];
+
+            #region Array init and input
+            Console.WriteLine("Enter "+a_length+" numbers");
+            for(int i=0;i<a_length;i++)
+            {
+                if (int.TryParse(Console.ReadLine(), out a[i]));
+                else a[i] = 0;
+            }
+            #endregion
+
+            #region Array output
+            Console.WriteLine("Output:");
+            foreach(int x in a)
+            {
+                Program obj = new Program(x);
+                Console.WriteLine(obj.Addable());
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
